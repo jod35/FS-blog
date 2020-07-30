5c013fcbda509e9a95aba168b08c42553c817de4
jod35 <jodestrevin@gmail.com>
2020-07-30 04:49:15 -0700

    add(class methods to user model)

--- main/api/views.py
+++ main/api/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint,jsonify,make_response,request
 
 api_bp=Blueprint('api_bp',__name__)
 
 @api_bp.route('/')
 def hello():
-    return "Hello"
+    return make_response(jsonify({"message":"Hey Welcome to the blog API" }),200)
+
